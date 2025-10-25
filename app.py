8596257bc96357f95099c9a25c5f36a047e39d4f
ahk9294 <ahk9294@gmail.com>
2025-10-25 12:13:19 +0530

    fixed docker host port

--- app.py
+++ app.py
@@ -118,5 +118,4 @@ def ask_question():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() # Create the database and tables
-    # Bind to 0.0.0.0 so the app is accessible from the Docker host/container mapping
     app.run(host="0.0.0.0", port=5000,debug=True)
