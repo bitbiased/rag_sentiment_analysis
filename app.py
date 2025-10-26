135abd13704ab9663160e9c1fded6a10c21e5675
ahk9294 <ahk9294@gmail.com>
2025-10-26 10:23:08 +0530

    debugger switched off

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def ask_question():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() # Create the database and tables
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000)
